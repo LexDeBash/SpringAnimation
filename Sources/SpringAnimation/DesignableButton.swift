11917cc4cc6e1cef74d2d097ac9adc41506c4e9f
Alexey Efimov <lex.efimov@gmail.com>
2022-04-23 14:50:51 +0300

    Add DesignableButton

---
+++ Sources/SpringAnimation/DesignableButton.swift
@@ -0,0 +1,54 @@
+//
+//  DesignableButton.swift
+//  
+//
+//  Created by Alexey Efimov on 18.04.2022.
+//
+
+import UIKit
+
+@IBDesignable public class DesignableButton: SpringButton {
+
+    @IBInspectable public var borderColor = UIColor.clear {
+        didSet {
+            layer.borderColor = borderColor.cgColor
+        }
+    }
+    
+    @IBInspectable public var borderWidth: CGFloat = 0 {
+        didSet {
+            layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable public var cornerRadius: CGFloat = 0 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    @IBInspectable public var shadowColor = UIColor.clear {
+        didSet {
+            layer.shadowColor = shadowColor.cgColor
+        }
+    }
+    
+    @IBInspectable public var shadowRadius: CGFloat = 0 {
+        didSet {
+            layer.shadowRadius = shadowRadius
+        }
+    }
+    
+    @IBInspectable public var shadowOpacity: CGFloat = 0 {
+        didSet {
+            layer.shadowOpacity = Float(shadowOpacity)
+        }
+    }
+    
+    @IBInspectable public var shadowOffsetY: CGFloat = 0 {
+        didSet {
+            layer.shadowOffset.height = shadowOffsetY
+        }
+    }
+
+}
